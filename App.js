713e48c1dad093aaa98530f3d788cd10f2cb3624
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2019-04-01 14:38:59 +0900

    make input component

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,
+        StatusBar, TextInput, Dimensions, Platform, 
+        ScrollView } from 'react-native';
+// import styled from 'styled-components';
+import Todo from './Todo';
+
+const { height , width } = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    NewTodo : ""
+  };  
   render() {
+    const {NewTodo} = this.state;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.Container}>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.Title}>Native TODO</Text>
+        <View style={styles.Card}>
+          <TextInput 
+          style={styles.Input} 
+          placeholder={"New ToDo"} 
+          value={Text}
+          onChangeText={this._FillText}
+          placeholderTextColor={"#999"}
+          returnKeyType={"done"}
+          autoCorrect={false}/>
+
+        <ScrollView contentContainerStyle={styles.Todos}>
+          <Todo/>
+        </ScrollView>
+        </View>
       </View>
     );
   }
+  _FillText = text => {
+    this.setState({
+      NewTodo : text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  Container : {
+    flex : 1,
+    backgroundColor : "rgb(234, 58, 90)",
+    alignItems : "center"
   },
+  Title : {
+    color : "white",
+    fontSize : 30,
+    marginTop : 50,
+    fontWeight : "300"
+  },
+  Card : {
+    backgroundColor : "white",
+    marginTop : 20,
+    flex :1,
+    width : width-25,
+    borderTopLeftRadius : 10,
+    borderTopRightRadius : 10,
+    ...Platform.select({
+      android : {
+        elevation : 5
+      }
+    })
+  },
+  Input : {
+    padding : 20,
+    borderBottomColor : "#bbb",
+    borderBottomWidth : StyleSheet.hairlineWidth,
+    fontSize : 25
+  },
+  Todos : {
+    alignItems : "center"
+  }
 });
+
---
+++ Todo.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet,
+        Dimensions } from 'react-native';
+
+const { width , height } = Dimensions.get("window");
+
+export default class Todo extends React.Component{
+    state = {
+        isEditing : false,
+        isCompleted : false 
+    };
+    render(){
+        const {isCompleted} = this.state;
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity onPress={this._toggleComplete}>
+                    <View style={[
+                        styles.circle,
+                        isCompleted ? styles.completedCircle : styles.uncompletedCircle
+                        ]}/>
+                </TouchableOpacity>
+                <Text>Hello</Text>
+            </View>
+        );
+    }
+    _toggleComplete = () => {
+        this.setState(prevState => {
+            return ({
+                isCompleted : !prevState.isCompleted
+            })
+        })
+    }
+}
+
+const styles = StyleSheet.create({
+    container : {
+        width : width -50,
+        borderBottomColor : "#bbb",
+        borderBottomWidth : StyleSheet.hairlineWidth,
+        flexDirection : "row",
+        alignItems : "center"
+    },
+    text : {
+        fontWeight : "600",
+        fontSize : 20,
+        marginVertical : 20,
+        
+    },
+    circle : {
+        width : 30,
+        height : 30,
+        borderRadius : 15,
+        borderColor : "red",
+        borderWidth : 3,
+        marginRight : 20
+    },
+    completedCircle : {
+        borderColor : "#bbb"
+    },
+    uncompletedCircle : {
+        borderColor : "#F23657"
+    }
+})
